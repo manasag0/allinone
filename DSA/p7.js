53cad3f15a412787fc710b3480d4dd96f3c32681
Manasa G <manasa.g7890@hotmail.com>
2024-04-06 18:42:29 +0530

    tarnery templates

--- DSA/p7.js
+++ DSA/p7.js
@@ -1,2 +1,26 @@
 //day8 // day63 //62left
 //6.4.24
+
+// odd or even 
+// ternary operator
+function isOddOrEven(num){
+return  num % 2 ===0 ? "even" : "odd";
+}
+
+let num = 7;
+console.log(isOddOrEven(num));
+
+//Switch oparator
+
+let day =  "Monday";
+switch (day) {
+    case "Monday":
+        console.log("It's Monday");
+        break;
+    case "Tuesday":
+        console.log("It's Tuesday");
+        break;
+    default:
+}
+//Array manipulation
+ 
