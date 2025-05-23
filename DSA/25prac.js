074feefc374ba20b7b8f75a647b7f64375b9b101
Manasa G <manasa.g7890@hotmail.com>
2025-05-23 19:36:10 +0530

    random update

---
+++ DSA/25prac.js
@@ -0,0 +1,12 @@
+let a = 2;
+let table;
+for(let i = 2;i<=10;i++){
+    for(j=2; j<=10;j++){
+    table = `${i} * ${j} = ${j * i}`;
+    
+    console.log(table);
+    }
+    console.log(" ");
+}
+
+
